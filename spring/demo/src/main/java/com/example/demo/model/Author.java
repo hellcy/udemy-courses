e58642aced87bfaccb61b1f73cc395364ef7b472
hellcy <chengyuan82281681@hotmail.com>
2021-10-29 14:39:44 +1100

    equality in Hibernate

--- spring/demo/src/main/java/com/example/demo/model/Author.java
+++ spring/demo/src/main/java/com/example/demo/model/Author.java
@@ -6,56 +6,89 @@ import java.util.Set;
 @Entity
 public class Author {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
 
-    private String firstName;
-    private String lastName;
+  private String firstName;
+  private String lastName;
 
-    @ManyToMany(mappedBy = "authors")
-    private Set<Book> books;
+  @ManyToMany(mappedBy = "authors")
+  private Set<Book> books;
 
-    public Author() {
-    }
+  public Author() {
+  }
 
-    public Author(String firstName, String lastName, Set<Book> books) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.books = books;
-    }
+  public Author(String firstName, String lastName, Set<Book> books) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.books = books;
+  }
 
 
 
-    public Long getId() {
-        return id;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public String getFirstName() {
-        return firstName;
-    }
+  public String getFirstName() {
+    return firstName;
+  }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
 
-    public String getLastName() {
-        return lastName;
-    }
+  public String getLastName() {
+    return lastName;
+  }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
 
-    public Set<Book> getBooks() {
-        return books;
-    }
+  public Set<Book> getBooks() {
+    return books;
+  }
 
-    public void setBooks(Set<Book> books) {
-        this.books = books;
-    }
+  public void setBooks(Set<Book> books) {
+    this.books = books;
+  }
+
+  /*
+    return object information when cast it to string
+   */
+  @Override
+  public String toString() {
+    return "Author{" +
+            "id=" + id +
+            ", firstName='" + firstName + '\'' +
+            ", lastName='" + lastName + '\'' +
+            ", books=" + books +
+            '}';
+  }
+
+  /*
+    Override the equals and hashcode functions
+    1. they are equal when their ids are the same
+    2. return the id as the hashcode
+  */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    Author author = (Author) o;
+
+    return id != null ? id.equals(author.id) : author.id == null;
+  }
+
+  @Override
+  public int hashCode() {
+    return id != null ? id.hashCode() : 0;
+  }
 }
